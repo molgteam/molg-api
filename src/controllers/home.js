780a3aa2f53ae79a5bdc6ee0ddc0888c5dbadb2b
Martin Jason <martinjason@Martinui-MacBookPro.local>
2020-06-15 00:46:24 +0900

    1

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,4 @@
 exports.index = function (req, res) {
-  res.render('MainPage', { page_name: 'Page1' });
+  // res.render('MainPage', { page_name: 'Page1' });
+  res.json({ author: "WOOK & GGUN" });
 };
