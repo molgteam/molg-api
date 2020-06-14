5a61b25785679f4f17a8c925e2eaa3f4afbb2827
Martin Jason <martinjason@Martinui-MacBookPro.local>
2020-06-15 00:30:57 +0900

    1

--- src/app.js
+++ src/app.js
@@ -1,88 +1,80 @@
-const path = require('path');
-const express = require('express');
-const config = require('../webpack.config');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+const path = require("path");
+const express = require("express");
+const config = require("../webpack.config");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
 const compiler = webpack(config);
 const app = express();
-const rateLimit = require('express-rate-limit');
 
-const { createLog } = require('../src/services/DBService');
+const { createLog } = require("../src/services/DBService");
 
-const apiLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
-  max: 10,
-  message:
-    '당신의 IP에서 너무 많은 요청을 한다 찍찍.... 10분 뒤에 다시와라 찍찍.. ',
-});
-
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 // routes
-const home = require('./routes/home');
-const user = require('./routes/user');
-const feed = require('./routes/feed');
-const story = require('./routes/story');
-const apiRoutes = require('./routes/api');
+const home = require("./routes/home");
+const user = require("./routes/user");
+const feed = require("./routes/feed");
+const story = require("./routes/story");
+const apiRoutes = require("./routes/api");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // 뷰엔진 셋업
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 app.use(
   express.static(
     path.join(
       __dirname,
-      process.env.NODE_ENV !== 'production' ? 'public' : '../dist',
-    ),
-  ),
+      process.env.NODE_ENV !== "production" ? "public" : "../dist"
+    )
+  )
 ); // 정적 파일 경로
 
 // hot-module 셋업
 // 정적소스 hmw랑 연결함. 정적 소스 저장할 때 마다 새로고침됨.
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   app.use(
     webpackDevMiddleware(compiler, {
       publicPath: config.output.publicPath,
       noInfo: true,
-    }),
+    })
   );
   app.use(webpackHotMiddleware(compiler));
 }
 
 // 페이지 라우팅
-app.use('/', home, apiLimiter);
-app.use('/user', user, apiLimiter);
-app.use('/feed', feed, apiLimiter);
-app.use('/story', story, apiLimiter);
+app.use("/", home);
+app.use("/user", user);
+app.use("/feed", feed);
+app.use("/story", story);
 
 // api 라우팅
-app.use('/api/v1/feed', apiRoutes.feed, apiLimiter);
-app.use('/api/v1/story', apiRoutes.refreshStory, apiLimiter);
+app.use("/api/v1/feed", apiRoutes.feed);
+app.use("/api/v1/story", apiRoutes.refreshStory);
 
 // 없는 경로 진입시
 app.use((req, res) => {
-  res.send('Not Found');
+  res.send("Not Found");
 });
 
 // error handler
 app.use(async function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   await createLog(err.message);
-  res.render('Error');
+  res.render("Error");
 });
 
 module.exports = app;
--- src/services/DBService.js
+++ src/services/DBService.js
@@ -1,13 +1,13 @@
-const knex = require('../../config/database');
-const { getCurrentUnixDate } = require('../services/utilities');
+const knex = require("../../config/database");
+const { getCurrentUnixDate } = require("../services/utilities");
 
 const createUser = async (user) => {
   let prevUser;
   let processedUser;
   try {
-    prevUser = await knex('user').where({ pk: user.pk });
+    prevUser = await knex("user").where({ pk: user.pk });
     if (prevUser.length) {
-      await knex('user')
+      await knex("user")
         .update({
           name: user.username,
           fullName: user.full_name,
@@ -18,133 +18,133 @@ const createUser = async (user) => {
         })
         .where({ pk: user.pk });
     } else {
-      await knex('user').insert({
+      await knex("user").insert({
         pk: user.pk,
-        name: user.username || '',
-        fullName: user.full_name || '',
+        name: user.username || "",
+        fullName: user.full_name || "",
         isPrivate: user.is_private,
         isVerified: user.is_verified,
         picUrl: user.profile_pic_url,
       });
     }
-    processedUser = await knex('user').where({ pk: user.pk });
+    processedUser = await knex("user").where({ pk: user.pk });
     return processedUser[0];
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const updateUserUpdatedAt = async (pk) => {
   try {
     let lastestUpdatedAt;
-    await knex('user')
+    await knex("user")
       .update({ updatedAt: getCurrentUnixDate() })
       .where({ pk });
 
-    lastestUpdatedAt = await knex('user').where({ pk });
+    lastestUpdatedAt = await knex("user").where({ pk });
     return lastestUpdatedAt[0];
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const fetchUserUpdatedAt = async (pk) => {
   try {
-    let user = await knex('user').where({ pk });
+    let user = await knex("user").where({ pk });
     return user[0].updatedAt;
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const fetchUserinfo = async (username) => {
   try {
-    let prevUser = await knex('user').where({ name: username });
+    let prevUser = await knex("user").where({ name: username });
 
     return prevUser[0];
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const createStorySnapshot = async (stories, user) => {
   try {
-    await knex('story').where({ userPk: user.pk }).del();
+    await knex("story").where({ userPk: user.pk }).del();
     // let prevUser = await knex('user').where({ pk: user.pk });
     // let latestUpdatedTime = prevUser[0].updatedAt;
-    await knex('story').insert(
+    await knex("story").insert(
       stories.map((story) => ({
         userPk: user.pk,
         pk: story.pk,
         hasVideo: story.has_video,
-        imageUrl: story.image_versions2[0].url || '',
-        videoUrl: story.has_video ? story.video_versions[0].url : '',
+        imageUrl: story.image_versions2[0].url || "",
+        videoUrl: story.has_video ? story.video_versions[0].url : "",
         takenAt: story.taken_at,
-        expireAt: story.expiring_at,
-      })),
+        expireAt: story.expiring_at,1
+      }))
     );
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const fetchStorySnapshot = async (user) => {
   try {
-    let stories = await knex('story').where({ userPk: user.pk });
+    let stories = await knex("story").where({ userPk: user.pk });
     return stories;
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const createLog = async (msg) => {
   try {
-    await knex('log').insert({
+    await knex("log").insert({
       message: msg,
       timestamp: getCurrentUnixDate(),
     });
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const fetchWorkers = async () => {
   try {
-    let workers = await knex('account').where({ isWorked: 1 });
+    let workers = await knex("api_account").where({ isWorked: 1 });
     return workers;
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const fetchWorkerByRandom = async () => {
   try {
     let worker = await knex.raw(
-      `SELECT * FROM account WHERE isWorked = '1' ORDER BY RAND() LIMIT 1`,
+      `SELECT * FROM api_account WHERE isWorked = '1' ORDER BY RAND() LIMIT 1`
     );
     return worker[0][0];
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const updateWorkerErrorCnt = async (workerUsername) => {
   try {
     await knex.raw(
-      `UPDATE account SET errorCnt = errorCnt + 1 WHERE username = '${workerUsername}'`,
+      `UPDATE api_account SET errorCnt = errorCnt + 1 WHERE username = '${workerUsername}'`
     );
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
 const updateWorker = async (workerUsername, worked) => {
   try {
-    await knex('account')
+    await knex("api_account")
       .update({ isWorked: String(worked) })
       .where({ username: workerUsername });
   } catch (e) {
-    throw new Error('찌리리리리지ㅣ지지지 으아아악 감전이다.', e);
+    throw new Error("찌리리리리지ㅣ지지지 으아아악 감전이다.", e);
   }
 };
 
